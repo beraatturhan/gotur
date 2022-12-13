a337fa5d04e7a5481f7c898189d33da0daea55af
beraa <beraa@LAPTOP-EAEJ1U8G>
2022-12-13 22:08:08 +0300

    Proje dosyası ekle.

---
+++ gotur/User/User.Master.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace gotur.User
+{
+    public partial class Site1 : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Request.Url.AbsoluteUri.ToString().Contains("Default.aspx"))
+            {
+                form1.Attributes.Add("class", "sub_page");
+            }
+            else
+            {
+                form1.Attributes.Remove("class");
+                Control SliderUserControl = (Control)Page.LoadControl("SliderUserControl.ascx");
+                pnlSliderUC.Controls.Add(SliderUserControl);
+            }
+
+        }
+    }
+}
