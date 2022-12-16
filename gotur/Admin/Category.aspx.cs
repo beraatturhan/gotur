31f735f3ad959626682eb12bc6d2988e78b58d60
bayra <veyselbayrakci@gmail.com>
2022-12-16 20:10:02 +0300

    Second Commit

---
+++ gotur/Admin/Category.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace gotur.Admin
+{
+    public partial class Category : System.Web.UI.Page
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataAdapter sda;
+        DataTable dt;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnAddorUpdate_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ gotur/Connection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Configuration;
+using System.Web;
+
+namespace gotur
+{
+    public class Connection
+    {
+        public static string GetConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["cs"].ConnectionString;
+        }
+    }
+}
